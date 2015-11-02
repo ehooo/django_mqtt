ce73a13e13a02983d8bf0c33179ef61b8ff5f5a1
Victor Torre <web.ehooo@gmail.com>
2015-11-02 16:02:55 +0100

    Fix requirements and settings

--- test_web/settings.py
+++ test_web/settings.py
@@ -86,6 +86,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR,  'templates'),
