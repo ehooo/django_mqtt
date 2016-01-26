78420333c16a1e0ed2da52cc2da6aa7cb0d2448c
Victor Torre <web.ehooo@gmail.com>
2016-01-26 01:33:45 +0100

    Fix travis

--- script/install_mosquitto_auth_plugin.sh
+++ script/install_mosquitto_auth_plugin.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo apt-get install mosquitto libssl-dev libmosquitto-dev libcurl4-openssl-dev -y
+sudo apt-get install mosquitto libssl-dev libmosquitto-dev libcurl4-openssl-dev -y -qq
