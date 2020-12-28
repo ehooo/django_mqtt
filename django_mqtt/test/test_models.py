08cab1db149fbd3f0a8a32b85c5b26a6bfc3cfec
ehooo <web.ehooo@gmail.com>
2020-12-28 19:39:06 +0100

    Fix test

--- django_mqtt/test/test_models.py
+++ django_mqtt/test/test_models.py
@@ -207,7 +207,7 @@ class TopicModelsTestCase(TestCase):
         topic = Topic(name='topic')
         self.assertEqual(0, Topic.objects.count())
         self.assertRaisesMessage(
-            TypeError,
+            AssertionError,
             "Topic object can't be deleted because its id attribute is set to None.",
             topic.delete
         )
